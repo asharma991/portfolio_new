9f7a0792ed36afb1aba0ab9a3140efb4e6589ee9
Abhishek <asharma991@outlook.com>
2025-02-22 03:55:17 +0530

    remove ts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3001,
-    open: true
-  }
+  plugins: [],
+  base: '/portfolio_new/'
 });
